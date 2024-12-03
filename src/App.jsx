d1bd3ce3bb1ffcfa61bbf2e4ac5b02e2622ba18f
Alex-Alessi <alex.alessi246@gmail.com>
2024-12-03 09:39:26 +0100

    work complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-// import { useState } from "react";
-import Header from "./components/Header";
-import Main from "./components/Main";
+import { useState } from "react";
+import Header from "./components/layout/Header";
+import Main from "./components/layout/Main";
 
 function App() {
   return (
